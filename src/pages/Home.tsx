d9d0eb2484dd11872d416ba22ac7c8c22f66218c
Muhamad Rizki <37399436+m-rizki@users.noreply.github.com>
2023-10-22 08:07:13 +0700

    Add Favorite Contact functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,31 +20,75 @@ const paginationStyle = css`
 const PAGE_SIZE = 10;
 
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [page, setPage] = useState(0);
+  const [contacts, setContacts] = useState<Contact[] | []>([]);
+  const [favContacts, setfavContacts] = useState<Contact[] | []>([]);
 
-  const { loading, error, data } = useQuery(GET_CONTACT_LIST, {
+  const getFavoriteContacts = (): Contact[] | [] => {
+    const storedFavoriteContactsJSON =
+      localStorage.getItem("favorite_contacts");
+    if (storedFavoriteContactsJSON) {
+      return JSON.parse(storedFavoriteContactsJSON);
+    } else {
+      return [];
+    }
+  };
+
+  const { loading, error, refetch } = useQuery(GET_CONTACT_LIST, {
     variables: {
       limit: PAGE_SIZE,
       offset: page * PAGE_SIZE,
       order_by: [{ first_name: "asc" }],
     },
-  });
+    fetchPolicy: "network-only",
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (queryData) => {
+      const contactsQuery: Contact[] = queryData?.contact || [];
+      const favContact = getFavoriteContacts();
+      setfavContacts(favContact);
 
-  const contacts: Contact[] = data?.contact || [];
+      // Filter out contacts that are also in favContact
+      const filteredContacts = contactsQuery.filter((contact) => {
+        return !favContact.some((fav) => fav.id === contact.id);
+      });
+      setContacts(filteredContacts);
+    },
+  });
 
   return (
     <section>
       <div css={titleContainerStyle}>
-        <h1>Contacts</h1>
-        <button title="Add Contact" css={buttonStyle} onClick={() => navigate("/contact/add")}>+</button>
+        <h1>Favorite</h1>
       </div>
       <Divider />
+      <Contacts
+        items={favContacts}
+        refetchContacts={() => refetch()}
+        isFavorite={true}
+      />
+
+      <div css={titleContainerStyle}>
+        <h1>Contacts</h1>
+        <button
+          title="Add Contact"
+          css={buttonStyle}
+          onClick={() => navigate("/contact/add")}
+        >
+          +
+        </button>
+      </div>
+      <Divider />
+
       {loading && <p>Loading...</p>}
       {error && <p css={errorFlag}>Error: {error.message}</p>}
 
-      <Contacts items={contacts} />
+      <Contacts
+        items={contacts}
+        refetchContacts={() => refetch()}
+        isFavorite={false}
+      />
 
       <div css={paginationStyle}>
         <button
